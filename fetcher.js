169ada08bf86e85f85b1997561350be231588521
Colin Poon <me@colinpoon.com>
2021-09-22 23:52:53 +0000

    fetcher without edge cases

--- fetcher.js
+++ fetcher.js
@@ -3,29 +3,21 @@ const request = require('request');
 
 args = process.argv.slice(2);
 
-request('https://www.google.com/fdsafsafsa.html', (error, response, body) => {
-
-});
-
-
-
-
-// const content = 'Some content!'
-
-// fs.writeFile('/Users/joe/test.txt', content, err => {
-//   if (err) {
-//     console.error(err)
-//     return
-//   }
-//   //file written successfully
-// })
-
-//EXPECTED OUTPUT
-//Downloaded and saved 1235 bytes to ./index.html.
-
-//(`Dowloaded and saved ${size} to ${args});
-
-// fs.write(fd, buffer[, offset[, length[, position]]], callback)
-//Write buffer to the file specified by fd. If buffer is a normal object, it must have an own toString function property.
-
-// offset determines the part of the buffer to be written, and length is an integer specifying the number of bytes to write.
+const fetch = function (url, page) {
+  request(url, (error, response) => {
+    // console.log('error:', error);
+    // console.log('statusCode:', response && response.statusCode);
+    // console.log(response.body);
+    if (response) { 
+      fs.writeFile(page, response.body, (err) => {
+        if (err){
+          console.log(err);
+          return
+        }
+        console.log(`Dowloaded and saved ${response.body.length} bytes to ${page}`);
+      })
+    }
+  });
+};
+let input = args;
+fetch(input[0], input[1]);
